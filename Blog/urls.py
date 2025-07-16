29e1b112a90a4e244e6bb9b49b10588a82679659
Beyzanur <beyzanurdincer502@gmail.com>
2025-07-16 21:08:50 +0300

    added profile

--- Blog/urls.py
+++ Blog/urls.py
@@ -1,25 +1,12 @@
 from django.contrib import admin
-from django.urls import path,include
+from django.urls import path, include
+from django.conf import settings
 from django.conf.urls.static import static
-from Blog.Blog import settings
-
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('',include('blogapp.urls'))
-    
+    path('', include('blogapp.urls')),  # Uygulamanın URL'leri burada çağrılır
 ]
-from django.urls import path 
-from . import views 
 
-urlpatterns = [
-    path('',views.home,name='home'),
-    path('post/<int:id>/',views.post_detail,name='post_detail'),
-    path('post/new/',views.post_new,name='post_new'),
-    path('post/<int:id>/edit/',views.post_edit,name='post_edit'),
-    path('post/<int:id>/delete/', views.post_delete, name='post_delete')
-
-    
-]
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
